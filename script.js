f522198813f69f06a2c0baf1223db63c9d4226a2
Yabiti <yabetseyohannes@gmail.com>
2025-06-04 11:10:02 +0300

    .

--- script.js
+++
@@ -1,7 +0,0 @@
-function showBack() {
-    document.getElementById("tshirtImage").src = "lady.jpg"; // Show back view on hover
-}
-
-function showFront() {
-    document.getElementById("tshirtImage").src = "lady.jpeg"; // Return to front view when mouse leaves
-}
